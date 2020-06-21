26e20ae3c2ec0cf67ef7ad4e5d35329b02adae4b
Teodor Tenchev <teodortenchev@gmail.com>
2020-06-21 20:31:47 +0400

    fixing changes caused by merge conflicts

--- controllers/cubes.js
+++ controllers/cubes.js
@@ -1,37 +1,16 @@
 const Cube = require('../models/cube');
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-
-const getAllCubes = (callback) => {
-    getCubes((cubes) => {
-        callback(cubes)
-    })
-=======
 const getAllCubes = async () => {
     const cubes = await Cube.find().lean();
     return cubes;
-}
-
-const getCube = async (id) => {
-    const cube = await Cube.findById(id).lean();
-    console.log(`Cube with id ${id}: `, cube)
-    return cube;
->>>>>>> Stashed changes
-=======
-const getAllCubes = async () => {
-    const cubes = await Cube.find().lean();
-    console.log(cubes);
-    return cubes;
-}
+};
 
 const getCube = async (id) => {
     const cube = await Cube.findById(id).lean();
     return cube;
->>>>>>> 73f88cc0fe4606009270716c41fa72b1c1b7b93a
-}
+};
 
 module.exports = {
     getAllCubes,
     getCube
-}
+};
--- routes/index.js
+++ routes/index.js
@@ -43,23 +43,11 @@ router.post('/create', (req, res) => {
             res.redirect('/');
         }
     });
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-})
-=======
 });
 
 router.get('/details/:id', async (req, res) => {
 
     const cube = await getCube(req.params.id);
->>>>>>> Stashed changes
-=======
-});
-
-router.get('/details/:id', async (req, res) => {
->>>>>>> 73f88cc0fe4606009270716c41fa72b1c1b7b93a
-
-    const cube = await getCube(req.param.id);
 
     res.render('details', {
         title: 'Details',
